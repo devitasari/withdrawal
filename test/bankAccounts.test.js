ce23d1169fcf9c3f2080c66719c66ff824a46bb9
devita <devita@devitas-MacBook-Pro.local>
2021-05-03 22:12:59 +0700

    add test after func

--- test/bankAccounts.test.js
+++ test/bankAccounts.test.js
@@ -1,4 +1,29 @@
 const chai = require("chai")
 const chaiHttp = require("chai-http")
 const app = require("../app")
-const bankAccounts = require("../models/bankAccounts")
+const BankAccounts = require("../models/bankAccounts")
+const { ObjectId } = require('mongodb');
+
+const userId = ObjectId("60900e6d6c5acb11e5122b99")
+const invalidUserId = Object("60900e6d6c5acb11e5122b90")
+
+chai.use(chaiHttp)
+const expect = chai.expect
+
+after(done => {
+    BankAccounts.deleteMany()
+    .then(() => {
+        console.log(`testing: delete all bank accounts succes!`)
+    })
+    .catch((err) => {
+        console.log(`error: ${err}`)
+    })
+    .finally(() => {
+        done()
+    })
+})
+
+describe(`bank accounts routes`, () => {
+    
+})
+
